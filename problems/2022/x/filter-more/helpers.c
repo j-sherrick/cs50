d66a3d3b465c05024847e4c2efd694838bbcd258
Jason Sherrick <jasonsherrick@gmail.com>
2022-10-21 23:44:11 -0500

    Change gray_avg to WORD

--- problems/2022/x/filter-more/helpers.c
+++ problems/2022/x/filter-more/helpers.c
@@ -9,7 +9,7 @@
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE *pixel = NULL;
-    BYTE gray_avg;
+    WORD gray_avg;
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
