3267e392b83ffca161c92f667b8c17874b571c67
Jason Sherrick <jasonsherrick@gmail.com>
2022-11-09 00:19:54 -0600

    Started debuging why I am only writing 1/3 of img

--- problems/2022/x/filter-more/helpers.c
+++ problems/2022/x/filter-more/helpers.c
@@ -2,6 +2,7 @@
 //#include "bmp.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 #define BOXSIZE 9
 
@@ -32,6 +33,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    printf("Image has height=%d width=%d\n", height, width);
     RGBTRIPLE imgblur[height][width];
     RGBTRIPLE *pixel = NULL;
 
@@ -44,7 +46,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int x = 0; x < width; x++)
         {
-            r_sum = 0;
+            //r_sum = 0;
             g_sum = 0;
             b_sum = 0;
             for (int i = 0; i < BOXSIZE; i++)
@@ -53,19 +55,28 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 int nY = y + yoff[i];
                 if (nX < 0 || nX >= width || nY < 0 || nY >= height)
                 {
+                    //printf("Neighbor offset out of bounds:\nnX=%d nY=%d, h=%d w=%d\n", nX, nY, height, width);
                     continue;
                 }
                 pixel = &image[nY][nX];
-                r_sum += pixel->rgbtRed;
+                //r_sum += pixel->rgbtRed;
                 g_sum += pixel->rgbtGreen;
                 b_sum += pixel->rgbtBlue;
             }
             pixel = &imgblur[y][x];
-            pixel->rgbtRed = r_sum / BOXSIZE;
+            pixel->rgbtRed = 250;
             pixel->rgbtGreen = g_sum / BOXSIZE;
             pixel->rgbtBlue = b_sum / BOXSIZE;
+            if (x == width - 1) printf("wrote the last pixel at x=%d y=%d\n", x, y);
         }
     }
+    FILE *out_ptr = fopen("./images/blur_dbg.bmp", "w");
+    if(!out_ptr)
+    {
+        printf("Oops! Couldn't open output file.\n");
+        return;
+    }
+    
     // Copy blurred image over to original
     for (int i = 0; i < height; i++)
     {
