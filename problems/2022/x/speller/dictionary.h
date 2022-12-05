3169e624a1983ad6b8bd2fa48cc51771c7edec02
Jason Sherrick <jasonsherrick@gmail.com>
2022-12-05 09:05:53 -0600

    Added variables to Makefile

--- problems/2022/x/speller/dictionary.h
+++ problems/2022/x/speller/dictionary.h
@@ -10,10 +10,10 @@
 #define LENGTH 45
 
 // Prototypes
-bool check(const char *word);
-unsigned int hash(const char *word);
-bool load(const char *dictionary);
-unsigned int size(void);
-bool unload(void);
+bool            check(const char *word);
+unsigned int    hash(const char *word);
+bool            load(const char *dictionary);
+unsigned int    size(void);
+bool            unload(void);
 
 #endif // DICTIONARY_H
